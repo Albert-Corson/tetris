326bf936b798ae1e48fd84738680852b0ea14e97
Albert Corson <albert.corson@epitech.eu>
2019-04-26 16:24:56 +0200

    Improved error messages

--- src/flags/read_flags.c
+++ src/flags/read_flags.c
@@ -13,9 +13,11 @@ int set_key(tetris_t *hub, char flag)
     int i = get_char_pos("lrtdqp", flag);
     int *keys[6] = {&ctrl->kleft, &ctrl->kright, &ctrl->krotate, &ctrl->kdrop, \
         &ctrl->kquit, &ctrl->kpause};
+    char *keyname[6] = {"left", "right", "turn", "drop", "quit", "pause"};
 
     if (my_strlen(optarg) != 1) {
-        my_dprintf(2, "set key: invalid argument \"%s\"\n", optarg);
+        my_dprintf(2, "tetris: --key-%s: invalid argument \"%s\"\n", \
+        keyname[i], optarg);
         return (84);
     }
     *keys[i] = optarg[0];
@@ -27,7 +29,7 @@ int set_map_size(tetris_t *hub)
     int i = get_char_pos(optarg, ',');
 
     if (i == -1 || !my_nisnum(optarg, i) || !my_nisnum(optarg + i + 1, -1)) {
-        my_dprintf(2, "set map size: invalid argument \"%s\"\n", optarg);
+        my_dprintf(2, "tetris: --map-size: invalid argument \"%s\"\n", optarg);
         return (84);
     }
     hub->map.size.y = my_atoi(optarg);
@@ -38,7 +40,7 @@ int set_map_size(tetris_t *hub)
 int set_level(tetris_t *hub)
 {
     if (!my_nisnum(optarg, -1)) {
-        my_dprintf(2, "set level: invalid argument \"%s\"\n", optarg);
+        my_dprintf(2, "tetris: --level: invalid argument \"%s\"\n", optarg);
         return (84);
     }
     hub->map.level = my_atoi(optarg);
@@ -69,14 +71,14 @@ int read_flags(int ac, char *const *av, tetris_t *hub)
     int n = 0;
     vector_t val = VECT(0, 0);
     const struct option opt[] = {{"help", no_argument, NULL, 'h'}, \
-        {"level=", required_argument, NULL, 'L'},
-        {"key-left=", required_argument, NULL, 'l'},
-        {"key-right=", required_argument, NULL, 'r'},
-        {"key-turn=", required_argument, NULL, 't'},
-        {"key-drop=", required_argument, NULL, 'd'},
-        {"key-quit=", required_argument, NULL, 'q'},
-        {"key-pause=", required_argument, NULL, 'p'},
-        {"map-size=", required_argument, NULL, 'm'},
+        {"level", required_argument, NULL, 'L'},
+        {"key-left", required_argument, NULL, 'l'},
+        {"key-right", required_argument, NULL, 'r'},
+        {"key-turn", required_argument, NULL, 't'},
+        {"key-drop", required_argument, NULL, 'd'},
+        {"key-quit", required_argument, NULL, 'q'},
+        {"key-pause", required_argument, NULL, 'p'},
+        {"map-size", required_argument, NULL, 'm'},
         {"without-next", no_argument, NULL, 'w'},
         {"debug", no_argument, NULL, 'D'},
         {0, 0, 0, 0}};
